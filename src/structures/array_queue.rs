d0d1b3c52215e4ab26efd385b239baa61df80516
gaofeng <gaofans@163.com>
2021-07-26 15:51:55 +0800

    vec队列

---
+++ src/structures/array_queue.rs
@@ -0,0 +1,69 @@
+use std::fmt::{Display, Formatter};
+#[derive(Debug)]
+pub struct ArrayQueue<T>{
+    max_size:usize,
+    size:usize,
+    queue:Vec<T>
+}
+
+impl <T> ArrayQueue<T> {
+    fn new(max_size:usize) -> ArrayQueue<T>{
+        ArrayQueue{ max_size,size:0,queue:Vec::with_capacity(max_size)}
+    }
+
+    fn add(&mut self,t:T) -> Result<&mut Self,String>{
+        if self.max_size == (self.size) {
+            return Err(String::from("队列已满"));
+        }
+        self.queue.push(t);
+        self.size += 1;
+        Ok(self)
+    }
+    fn take(&mut self) -> Result<T,String>{
+        if self.size == 0 {
+            return Err(String::from("队列为空"));
+        }
+        let node = self.queue.remove(0);
+        self.size -= 1;
+        return Ok(node);
+    }
+}
+impl <T> Display for ArrayQueue<T> where T:Display{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        if self.size == 0 {
+            write!(f,"{}","[]")
+        }else{
+            let x :Vec<String>= self.queue.iter().map(|ele| format!("{}",ele)).collect();
+            write!(f,"{:?}",x)
+        }
+
+    }
+}
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut queue:ArrayQueue<i32> = ArrayQueue::new(10);
+        for i in 0..11 {
+            match queue.add(i) {
+                Ok(q) => println!("{}",q),
+                Err(msg) => println!("{}",msg)
+            }
+        }
+        println!("{}",queue);
+        for i in 0..5 {
+            println!("{:?}",queue.take());
+        }
+        println!("{}",queue);
+        for i in 10..20 {
+            queue.add(i);
+        }
+        println!("{}",queue);
+        for i in 0..11 {
+            println!("{:?}",queue.take());
+        }
+        println!("{}",queue);
+    }
+}
--- src/structures/mod.rs
+++ src/structures/mod.rs
@@ -1,1 +1,2 @@
 pub mod sparse_array;
+pub mod array_queue;
