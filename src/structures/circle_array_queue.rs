4544505284c5dcf96d61802d29bc795590d8be4b
gaofeng <gaofans@163.com>
2021-07-27 18:14:00 +0800

    vec环形队列

---
+++ src/structures/circle_array_queue.rs
@@ -0,0 +1,83 @@
+use std::mem::take;
+
+#[derive(Debug)]
+pub struct Queue<T:Copy>{
+    head:usize,
+    tail:usize,
+    max_size:usize,
+    array:Vec<T>
+}
+
+impl <T:Copy> Queue<T>{
+
+    pub fn new(max_size:usize) -> Queue<T>{
+        Queue{
+            head:0,
+            tail:0,
+            max_size,
+            array:Vec::with_capacity(max_size+1)
+        }
+    }
+
+    pub fn size(&self) -> usize{
+        let offset = self.tail - self.head;
+        if offset >= 0 {
+            offset
+        }else{
+            offset + self.max_size + 1
+        }
+    }
+
+    pub fn add(&mut self,t:T) -> Result<(),&str>{
+        if self.size() == self.max_size {
+           return Err("队列已满");
+        }
+        if self.array.len() == self.tail {
+            self.array.push(t);
+        }else{
+            self.array[self.tail] = t;
+        }
+        self.tail = (self.tail + 1) % (self.max_size + 1);
+        Ok(())
+    }
+
+    pub fn take(&mut self) -> Result<T,&str>{
+        if self.size() == 0 {
+            return Err("队列为空");
+        }
+        let t = self.array.get(self.head).unwrap();
+        self.head = (self.head + 1) % (self.max_size + 1);
+        Ok(*t)
+    }
+}
+
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    #[test]
+
+    fn test() {
+        let mut q:Queue<usize> = Queue::new(10);
+        for i in 0..11 {
+            match q.add(i) {
+                Err(s) => println!("{}",s),
+                _ => println!("{:?}",q)
+            }
+
+        }
+        for i in 0..3 {
+            match q.take() {
+                Err(s) => println!("{}",s),
+                Ok(t) => println!("{}",t)
+            }
+        }
+        for i in 0..5 {
+            match q.add(i) {
+                Err(s) => println!("{}",s),
+                _ => println!("{:?}",q)
+            }
+        }
+
+    }
+}
--- src/structures/mod.rs
+++ src/structures/mod.rs
@@ -1,2 +1,3 @@
 pub mod sparse_array;
 pub mod array_queue;
+pub mod circle_array_queue;
